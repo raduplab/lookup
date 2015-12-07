cda2b107a87ed1012866d022951576f17160513f
Radup <radup@radup.net>
2015-12-08 05:42:09 +0900

    Update project

--- common/scripts/template.load.js
+++ common/scripts/template.load.js
@@ -11,12 +11,31 @@ template.fn = template.prototype = {
         if ( typeof( vars ) != "object" ){
             return null;
         }
+        this.vars = vars;
+    },
+    vars: {},
+    render: function( html ){
+        html = html || "*";
 
-        
+        if ( html == "*" ){
+            var t = document.getElementsByTagName( "*" );
+            for ( var i=0; i < t.length; i++ ){
+                if ( /^{(.*)}$/.text( radup( t[i] ).text().trim() ) ){
+                    vals = /^{(.*)}$/.exec( radup( t[i] ).text().trim() )[1];
+                    for ( key in this.vars ){
+                        if ( key == vals )
+                            radup( t[i] ).html( radup( t[i] ).text().replace( (new RegExp( "({" + vals + "})" )), this.vars[ vals ] )  );
+                    }
+                }
+            }
+        }
     }
-}
+};
+template.fn.init.prototype = template.fn;
 
 return template;
 })();
+
+window.template = template;
 
 })(window)
